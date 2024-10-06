1362a29069166237312a16d2670b09c43941544c
michalcynarski <cynarski60@gmail.com>
2024-10-06 17:48:02 +0200

    Initial

---
+++ get_data.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+def download_data(crypto_symbol: str, start: str, end: str, interval: str = "1d") -> pd.DataFrame:
+    """
+    Downloads data from Yahoo finances
+    :param crypto_symbol: symbol of cryptocurrency\
+    :type crypto_symbol: str
+    :param start: first date of data to download
+    :type start: str
+    :param end: last date of data to download
+    :type start: str
+    :param interval: interval of data to download
+    :type interval: str
+    :return: dataframe with crypto courses
+    :rtype: pd.DataFrame
+    """
+    return yf.download(crypto_symbol, start=start, end=end, interval=interval)
+
+
+
